8eca58d4880df0c0a5234da641217505791ca222
Andrej Mihajlov <andrej@nymtech.net>
2025-05-30 13:38:44 +0200

    Enable support for iOS

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(
     not(target_os = "macos"),
-    doc = "NOTE: This library is only supported on macOS."
+    doc = "NOTE: This library is only supported on macOS and iOS."
 )]
 
-#[cfg(target_os = "macos")]
+#[cfg(any(target_os = "macos", target_os = "ios"))]
 pub use darwin::*;
 
-#[cfg(target_os = "macos")]
+#[cfg(any(target_os = "macos", target_os = "ios"))]
 mod darwin;
